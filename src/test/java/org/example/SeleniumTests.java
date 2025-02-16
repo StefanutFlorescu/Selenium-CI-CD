6de38509dc7a086c695fd7833ad9996ccb30a67c
Stefanut Florescu <stefanut.florescu@gmail.com>
2025-02-16 20:13:04 +0200

    Title Test

---
+++ src/test/java/org/example/SeleniumTests.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTests {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Marius Florescu\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+        String pageTitle = driver.getTitle();
+        System.out.println("The title is: " + pageTitle);
+
+        assert pageTitle.equals("Google") : "Test failed! Expected title 'Google', but got '" + pageTitle + "'";
+
+        driver.quit();
+    }
+}
